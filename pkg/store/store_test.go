903a30f399c8aedae2863e578a8b6b081b644e06
Blake Mizerany <blake.mizerany@gmail.com>
2010-09-06 23:49:27 -0700

    ...

--- pkg/store/store_test.go
+++ pkg/store/store_test.go
@@ -3,6 +3,7 @@ package store
 import (
 	"container/vector"
 	"junta/assert"
+	"log"
 	"os"
 	"testing"
 )
@@ -32,15 +33,15 @@ func (r req) Less(y interface{}) bool {
 }
 
 type Store struct {
-	seqn    uint64
-	reqCh   chan req
+	seqn  uint64
+	reqCh chan req
 	waits *vector.Vector
 }
 
 func New() *Store {
 	s := &Store{
-		seqn:    0,
-		reqCh:   make(chan req),
+		seqn:  0,
+		reqCh: make(chan req),
 		waits: new(vector.Vector),
 	}
 
@@ -56,18 +57,21 @@ func (s *Store) Close() {
 func (s *Store) process() {
 	// NOTE:  There should never be an error in here
 	for r := range s.reqCh {
-		if r.mask & Wait != 0 {
+		log.Stdoutf("r:%v\n", r)
+		if r.mask&Wait != 0 {
+			log.Stdoutf("pw:%v\n", r)
 			s.waits.Push(r)
 		}
 
-		if r.mask & Set != 0 {
+		if r.mask&Set != 0 {
 			s.seqn++
 			// TODO: Create the path and set the body
 			for n, x := range *s.waits {
+				log.Stdoutf("n:%d x:%v\n", n, x)
 				w := x.(req)
 				if w.seqn <= s.seqn {
 					w.ch <- Reply{s.seqn, r.path, r.body}
-					if w.mask | Once != 0 {
+					if w.mask&Once != 0 {
 						s.waits.Delete(n)
 					}
 				}
@@ -76,27 +80,26 @@ func (s *Store) process() {
 	}
 }
 
-func (s *Store) Req(seqn uint64, path, body string, mask uint, ch chan<- Reply) {
+func (s *Store) req(seqn uint64, path, body string, mask uint, ch chan<- Reply) {
 	s.reqCh <- req{seqn, path, body, mask, ch}
 }
 
-func (s *Store) Wait(seqn uint64, path string) (Reply, os.Error) {
-	ch := make(chan Reply)
-	s.Req(seqn, path, "", Wait, ch)
-	return <-ch, nil
+func (s *Store) Wait(seqn uint64, path string, ch chan<- Reply) os.Error {
+	s.req(seqn, path, "", Wait, ch)
+	return nil
 }
 
 func (s *Store) Set(seqn uint64, path, body string) (Reply, os.Error) {
 	ch := make(chan Reply)
-	s.Req(seqn, path, body, Wait | Once | Set, ch)
+	s.req(seqn, path, body, Wait|Once|Set, ch)
 	return <-ch, nil
 }
-
 
 // Testing
 
 func TestStoreSetSimple(t *testing.T) {
 	s := New()
+	defer s.Close()
 
 	var got Reply
 
@@ -109,4 +112,89 @@ func TestStoreSetSimple(t *testing.T) {
 	assert.Equal(t, uint64(2), got.seqn)
 	assert.Equal(t, "/foo", got.path)
 	assert.Equal(t, "rab", got.body)
+}
+
+func TestStoreWaitSimple(t *testing.T) {
+	s := New()
+	defer s.Close()
+
+	ch := make(chan Reply, 2)
+
+	s.Wait(0, "/foo", ch)
+
+	setCh := make(chan Reply)
+	go func() {
+		set, _ := s.Set(1, "/foo", "bar")
+		setCh <- set
+	}()
+
+	exp := <-setCh
+	assert.Equal(t, exp, <-ch)
+}
+
+func TestStoreWaitAfterSet(t *testing.T) {
+	s := New()
+	defer s.Close()
+
+	ch := make(chan Reply, 2)
+
+	setCh := make(chan Reply)
+	go func() {
+		set, _ := s.Set(1, "/foo", "bar")
+		setCh <- set
+	}()
+
+	s.Wait(0, "/foo", ch)
+
+	exp := <-setCh
+	assert.Equal(t, exp, <-ch)
+}
+
+func TestStoreWaitFuture(t *testing.T) {
+	s := New()
+	defer s.Close()
+
+	ch := make(chan Reply, 2)
+
+	s.Wait(2, "/foo", ch)
+	go s.Set(1, "/foo", "bar")
+	go s.Set(2, "/foo", "rab")
+
+	got := <-ch
+	assert.Equal(t, uint64(2), got.seqn)
+	assert.Equal(t, "rab", got.body)
+}
+
+func TestStoreSetOutOfOrder(t *testing.T) {
+	s := New()
+	defer s.Close()
+
+	st1 := make(chan int)
+	st2 := make(chan int)
+	ch := make(chan Reply)
+
+	go func() {
+		<-st1
+		r, _ := s.Set(1, "/foo", "bar")
+		st1 <- 1
+		ch <- r
+	}()
+
+	go func() {
+		<-st2
+		r, _ := s.Set(2, "/foo", "rab")
+		st2 <- 1
+		ch <- r
+	}()
+
+	st2 <- 1
+	<-st2
+	st1 <- 1
+	<-st1
+
+	got := <-ch
+	assert.Equal(t, uint64(1), got.seqn)
+
+	got = <-ch
+	assert.Equal(t, uint64(2), got.seqn)
 }
