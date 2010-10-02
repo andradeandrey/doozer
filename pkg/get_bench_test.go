c5c20286e6fba9dbb512e8e6a180af318749dedf
Keith Rarick <kr@xph.us>
2010-10-02 04:23:27 -0700

    get valid benchmarking results
    The nice thing about Go's benchmark package is that it chooses N for
    you.

--- pkg/get_bench_test.go
+++ pkg/get_bench_test.go
@@ -6,10 +6,10 @@ import (
 	"junta/store"
 	"net"
 	"testing"
-	"log"
 )
 
 func BenchmarkGet(b *testing.B) {
+	b.StopTimer()
 	conn, err := net.Dial("tcp", "", "127.0.0.1:8046")
 	if err != nil {
 		panic(err)
@@ -17,7 +17,7 @@ func BenchmarkGet(b *testing.B) {
 	junta := proto.NewConn(conn)
 
 	b.StartTimer()
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < b.N; i++ {
 		id, err := junta.SendRequest("sget", "/j/local/ping")
 		if err != nil {
 			panic(err)
@@ -32,10 +32,10 @@ func BenchmarkGet(b *testing.B) {
 			panic(fmt.Sprintf("Invalid parts %v", parts))
 		}
 	}
-	b.StopTimer()
 }
 
 func BenchmarkSet(b *testing.B) {
+	b.StopTimer()
 	conn, err := net.Dial("tcp", "", "127.0.0.1:8046")
 	if err != nil {
 		panic(err)
@@ -43,7 +43,7 @@ func BenchmarkSet(b *testing.B) {
 	junta := proto.NewConn(conn)
 
 	b.StartTimer()
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < b.N; i++ {
 		id, err := junta.SendRequest("set", "/j/local/bench/set", "abc", store.Clobber)
 		if err != nil {
 			panic(err)
@@ -54,10 +54,8 @@ func BenchmarkSet(b *testing.B) {
 			panic(err)
 		}
 
-		log.Stdoutf("got response %v\n", parts)
 		if len(parts) != 1 && parts[0] != "pong" {
 			panic(fmt.Sprintf("Invalid parts %v", parts))
 		}
 	}
-	b.StopTimer()
 }
