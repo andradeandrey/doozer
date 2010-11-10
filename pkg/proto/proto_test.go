8bda8b5a5a8d0bffe5721318a7af51d57b52ca6e
Blake Mizerany <blake.mizerany@gmail.com>
2010-11-09 16:54:50 -0800

    ...

--- pkg/proto/proto_test.go
+++ pkg/proto/proto_test.go
@@ -4,17 +4,13 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"io"
 	"junta/assert"
 	"os"
 	"testing"
 )
 
-type WriteFlusher interface {
-	Flush() os.Error
-	Write([]byte) (int, os.Error)
-}
-
-func encode(w WriteFlusher, a ... interface{}) (err os.Error) {
+func encode(w io.Writer, a ... interface{}) (err os.Error) {
 	_, err = fmt.Fprintf(w, "*%d\r\n", len(a))
 	if err != nil {
 		return
@@ -24,20 +20,46 @@ func encode(w WriteFlusher, a ... interface{}) (err os.Error) {
 		switch v.(type) {
 		case string:
 			s := v.(string)
-			fmt.Fprintf(w, "$%d\r\n%s\r\n", len(s), s)
+			_, err = fmt.Fprintf(w, "$%d\r\n%s\r\n", len(s), s)
+			if err != nil {
+				return
+			}
 		case int:
 			i := v.(int)
-			fmt.Fprintf(w, ":%d\r\n", i)
+			_, err = fmt.Fprintf(w, ":%d\r\n", i)
+			if err != nil {
+				return
+			}
 		}
 	}
 
-	return w.Flush() 
+	return
 }
 
+// An io.Writer that will return os.EOF on the `n`th byte written
+type eWriter struct {
+	n int
+}
+
+func (e *eWriter) Write(p []byte) (n int, err os.Error) {
+	l := len(p)
+	e.n -= l
+	if e.n <= 0 {
+		return 0, os.EOF
+	}
+	return l, nil
+}
 
 func TestProtoEncode(t *testing.T) {
 	buf := new(bytes.Buffer)
 	w   := bufio.NewWriter(buf)
 	assert.Equal(t, nil, encode(w, "set", "foo", 1))
+	w.Flush()
 	assert.Equal(t, "*3\r\n$3\r\nset\r\n$3\r\nfoo\r\n:1\r\n", buf.String())
+}
+
+func TestProtoEncodeErrors(t *testing.T) {
+	assert.Equal(t, os.EOF, encode(&eWriter{4}))
+	assert.Equal(t, os.EOF, encode(&eWriter{5}, "ping"))
+	assert.Equal(t, os.EOF, encode(&eWriter{5}, 1))
 }
